137cf6d4983bc5b6ff20766f0a34f1acae099a76
Manuel Bitto <manuel.bitto@gmail.com>
2019-05-13 19:55:29 +0200

    format code

--- mini-batch-gradient-descent/batch_graddesc.py
+++ mini-batch-gradient-descent/batch_graddesc.py
@@ -6,11 +6,11 @@ np.random.seed(42)
 # TODO: Fill in code in the function below to implement a gradient descent
 # step for linear regression, following a squared error rule. See the docstring
 # for parameters and returned variables.
-def MSEStep(X, y, W, b, learn_rate = 0.005):
+def MSEStep(X, y, W, b, learn_rate=0.005):
     """
     This function implements the gradient descent step for squared error as a
     performance metric.
-    
+
     Parameters
     X : array of predictor features
     y : array of outcome values
@@ -22,8 +22,8 @@ def MSEStep(X, y, W, b, learn_rate = 0.005):
     W_new : predictor feature coefficients following gradient descent step
     b_new : intercept following gradient descent step
     """
-    
-    # Fill in code    
+
+    # Fill in code
     return W_new, b_new
 
 
@@ -31,7 +31,7 @@ def MSEStep(X, y, W, b, learn_rate = 0.005):
 # button. The gradient descent step will be performed multiple times on
 # the provided dataset, and the returned list of regression coefficients
 # will be plotted.
-def miniBatchGD(X, y, batch_size = 20, learn_rate = 0.005, num_iter = 25):
+def miniBatchGD(X, y, batch_size=20, learn_rate=0.005, num_iter=25):
     """
     This function performs mini-batch gradient descent on a given dataset.
 
@@ -47,31 +47,31 @@ def miniBatchGD(X, y, batch_size = 20, learn_rate = 0.005, num_iter = 25):
       descent procedure
     """
     n_points = X.shape[0]
-    W = np.zeros(X.shape[1]) # coefficients
-    b = 0 # intercept
-    
+    W = np.zeros(X.shape[1])  # coefficients
+    b = 0  # intercept
+
     # run iterations
-    regression_coef = [np.hstack((W,b))]
+    regression_coef = [np.hstack((W, b))]
     for _ in range(num_iter):
         batch = np.random.choice(range(n_points), batch_size)
-        X_batch = X[batch,:]
+        X_batch = X[batch, :]
         y_batch = y[batch]
         W, b = MSEStep(X_batch, y_batch, W, b, learn_rate)
-        regression_coef.append(np.hstack((W,b)))
-    
+        regression_coef.append(np.hstack((W, b)))
+
     return regression_coef
 
 
 if __name__ == "__main__":
     # perform gradient descent
-    data = np.loadtxt('data.csv', delimiter = ',')
-    X = data[:,:-1]
-    y = data[:,-1]
+    data = np.loadtxt('data.csv', delimiter=',')
+    X = data[:, :-1]
+    y = data[:, -1]
     regression_coef = miniBatchGD(X, y)
-    
+
     # plot the results
     import matplotlib.pyplot as plt
-    
+
     plt.figure()
     X_min = X.min()
     X_max = X.max()
@@ -79,6 +79,6 @@ if __name__ == "__main__":
     for W, b in regression_coef:
         counter -= 1
         color = [1 - 0.92 ** counter for _ in range(3)]
-        plt.plot([X_min, X_max],[X_min * W + b, X_max * W + b], color = color)
-    plt.scatter(X, y, zorder = 3)
+        plt.plot([X_min, X_max], [X_min * W + b, X_max * W + b], color=color)
+    plt.scatter(X, y, zorder=3)
     plt.show()
