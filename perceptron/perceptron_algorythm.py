73cc6fece0b8a1af1b4b13eeaebc3b0828c1ae41
Manuel Bitto <manuel.bitto@gmail.com>
2019-05-24 16:18:32 +0200

    add data to perceptron_algorythm

--- perceptron/perceptron_algorythm.py
+++ perceptron/perceptron_algorythm.py
@@ -12,7 +12,6 @@ def stepFunction(t):
 def prediction(X, W, b):
     return stepFunction((np.matmul(X, W)+b)[0])
 
-# TODO: Fill in the code below to implement the perceptron trick.
 # The function should receive as inputs the data X, the labels y,
 # the weights W (as an array), and the bias b,
 # update the weights and bias W, b, according to the perceptron algorithm,
@@ -20,7 +19,16 @@ def prediction(X, W, b):
 
 
 def perceptronStep(X, y, W, b, learn_rate=0.01):
-    # Fill in code
+    for i in range(len(X)):
+        y_hat = prediction(X[i], W, b)
+        if y[i]-y_hat == 1:
+            W[0] += X[i][0]*learn_rate
+            W[1] += X[i][1]*learn_rate
+            b += learn_rate
+        elif y[i]-y_hat == -1:
+            W[0] -= X[i][0]*learn_rate
+            W[1] -= X[i][1]*learn_rate
+            b -= learn_rate
     return W, b
 
 # This function runs the perceptron algorithm repeatedly on the dataset,
